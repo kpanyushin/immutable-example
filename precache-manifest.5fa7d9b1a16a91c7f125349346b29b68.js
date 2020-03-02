7e66f4895718a6122b5b12cdd616c408b79cf3d3
kpanyushin <kirill.panyushin@saltpepper.ru>
2020-03-02 15:13:46 +0300

    Updates

---
+++ precache-manifest.5fa7d9b1a16a91c7f125349346b29b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317e614bfe7d7d25d5d68e34082e9d22",
+    "url": "/immutable-example/index.html"
+  },
+  {
+    "revision": "1ab6694160fb04e7c0a7",
+    "url": "/immutable-example/static/css/main.73d27734.chunk.css"
+  },
+  {
+    "revision": "1d0c092b39d31e315f24",
+    "url": "/immutable-example/static/js/2.17725019.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/immutable-example/static/js/2.17725019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab6694160fb04e7c0a7",
+    "url": "/immutable-example/static/js/main.2ed7c37f.chunk.js"
+  },
+  {
+    "revision": "1db0bd72327fa604139a",
+    "url": "/immutable-example/static/js/runtime-main.46ac6383.js"
+  }
+]);
